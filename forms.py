5d047bb16f65b2463fd11bff52bf62b0cd132f7a
Gbenga Sorinola <sorinolag@gmail.com>
2021-08-22 14:29:02 +0200

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms.fields import CharField
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=50)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000)
-    
